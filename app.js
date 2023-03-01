00291087a814c356bc7c34fa810417b3d1407eaf
Azimet Kerem Ersoy <azimetkeremersoy@gmail.com>
2023-03-01 22:20:49 +0300

    commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+// middleware
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda başlatıldı`);
+});
