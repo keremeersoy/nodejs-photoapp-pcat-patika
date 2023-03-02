63f09ba182479d8b5aa538930d4d139a73f57919
Azimet Kerem Ersoy <azimetkeremersoy@gmail.com>
2023-03-02 03:05:22 +0300

    dynamic routes and contents

--- app.js
+++ app.js
@@ -1,14 +1,53 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const path = require("path");
+const ejs = require("ejs");
+const Photo = require("./models/Photo");
 
 const app = express();
 const port = 3000;
 
+// connect DB
+mongoose.connect("mongodb://127.0.0.1:27017/pcat-test-db", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// template engine
+app.set("view engine", "ejs");
+// mongoose.set("strictQuery", false);
+
 // middleware
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "temp/index.html"));
+// routes
+app.get("/", async (req, res) => {
+  const photos = await Photo.find({});
+  res.render("index", {
+    photos,
+  });
+});
+
+app.get("/photos/:id", async (req, res) => {
+  const photo = await Photo.findById(req.params.id);
+  res.render("photo", {
+    photo,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.post("/photos", async (req, res) => {
+  await Photo.create(req.body);
+  res.redirect("/");
 });
 
 app.listen(port, () => {
